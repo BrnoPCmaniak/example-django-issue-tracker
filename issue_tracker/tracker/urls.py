93266045fc615eda527f763b4c7a7397a919a0ea
Filip Dobrovolny <brnopcman@gmail.com>
2018-06-18 16:11:06 +0200

    Add issue list view

--- issue_tracker/tracker/urls.py
+++ issue_tracker/tracker/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
+from .views import ListIssueView
+
 urlpatterns = [
     path('accounts/login', auth_views.login,
          {'template_name': 'tracker/signin.html'}, name='login'
@@ -8,4 +10,5 @@ urlpatterns = [
     path('accounts/logout', auth_views.logout,
          {'next_page': '/accounts/login?logout=1'}, name='logout'
          ),
+    path('home', ListIssueView.as_view(), name="list"),
 ]
--- issue_tracker/tracker/views.py
+++ issue_tracker/tracker/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.db.models import Avg, Max, Min
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Issue
+
+
+class ListIssueView(ListView):
+    model = Issue
+    template_name = "tracker/list.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        numbers = Issue.objects.exclude(completed_in__isnull=True).aggregate(
+            Avg('completed_in'), Max('completed_in'), Min('completed_in'))
+        context["avg"] = numbers["completed_in__avg"]
+        context["min"] = numbers["completed_in__min"]
+        context["max"] = numbers["completed_in__max"]
+        return context
